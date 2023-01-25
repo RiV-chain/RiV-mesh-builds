d4dc721f9f3d6ac2a2bde14b21fcdf472a3fbfdd
Vadym Vikulin <vadym.vikulin@gmail.com>
2023-01-25 16:25:52 +0200

    Update generate-gui.sh

--- contrib/deb/generate-gui.sh
+++ contrib/deb/generate-gui.sh
@@ -23,7 +23,7 @@ if [ $PKGBRANCH = "master" ]; then
 fi
 
 buildbin() {
-  local CMD=$(realpath $1)
+  local CMD=$( cd "$(dirname "$1")" ; pwd -P )
   echo "Building: $CMD for $GOOS-$GOARCH"
 
   (cd "$TARGET_PATH" && go build $ARGS -ldflags "${LDFLAGS}${LDFLAGS2}" -gcflags "$GCFLAGS" "$CMD")
