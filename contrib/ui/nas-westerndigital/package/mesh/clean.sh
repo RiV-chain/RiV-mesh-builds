dc8ff4c4daa76d22fd1226c4ef2813682c129735
Vadym Vikulin <vadym.vikulin@rivchain.org>
2023-07-25 20:13:51 +0300

    Cleanup fix

--- contrib/ui/nas-westerndigital/package/mesh/clean.sh
+++ contrib/ui/nas-westerndigital/package/mesh/clean.sh
@@ -7,5 +7,6 @@ rm -f /usr/bin/mesh
 rm -f /usr/bin/meshctl
 
 
-rm -rf /var/www/mesh
 rm -f /usr/local/apache2/conf/extra/apache-mesh.conf
+rm -f /usr/local/apache2/conf/sites-enabled/apache-mesh.conf
+rm -rf /var/www/mesh_app 2>&1 >/dev/null
